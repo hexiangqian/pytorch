2936ff745ce59b408ecab234ddf25d977bf7cd9d
Ashkan Aliabadi <ashkanaliabadi@fb.com>
2020-08-16 16:29:12 -0700

    Vulkan command buffer and pool.
    ghstack-source-id: 59ae1f100efc7b76521370c47ecdbfc0cd39eb0d
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/42930

---
+++ aten/src/ATen/native/vulkan/api/Command.h
@@ -0,0 +1,85 @@
+#pragma once
+
+#include <ATen/native/vulkan/api/Common.h>
+#include <ATen/native/vulkan/api/Cache.h>
+#include <c10/util/hash.h>
+
+namespace at {
+namespace native {
+namespace vulkan {
+namespace api {
+
+struct Command final {
+  //
+  // Pool
+  //
+
+  struct Pool final {
+    /*
+      Descriptor
+    */
+
+    struct Descriptor final {
+      uint32_t queue_family_index;
+
+      inline bool operator==(const Descriptor& descriptor) const {
+        return queue_family_index == descriptor.queue_family_index;
+      }
+    };
+
+    /*
+      Factory
+    */
+
+    class Factory final {
+     public:
+      explicit Factory(VkDevice device);
+
+      typedef Pool::Descriptor Descriptor;
+      typedef VK_DELETER(CommandPool) Deleter;
+      typedef Handle<VkCommandPool, Deleter> Handle;
+
+      struct Hasher {
+        inline size_t operator()(const Descriptor& descriptor) const {
+          return c10::get_hash(descriptor.queue_family_index);
+        }
+      };
+
+      Handle operator()(const Descriptor& descriptor) const;
+
+     private:
+      VkDevice device_;
+    };
+
+    /*
+      Cache
+    */
+
+    typedef api::Cache<Factory> Cache;
+    Cache cache;
+  } pool;
+
+  //
+  // Buffer
+  //
+
+  class Buffer final {
+   public:
+    Buffer(VkDevice device, VkCommandPool command_pool);
+
+    void begin();
+    void end();
+
+    void bind(VkPipeline pipeline);
+    void bind(VkPipelineLayout pipeline_layout, VkDescriptorSet descriptor_set);
+    void dispatch();
+
+   private:
+    VkCommandBuffer command_buffer_;
+  };
+};
+
+} // namespace api
+} // namespace vulkan
+} // namespace native
+} // namespace at
